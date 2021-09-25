05db0ecb697165917bbe3f7609cfd10715db0595
unknown <johncloeter@gmail.com>
2021-09-24 20:27:15 -0500

    using useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // console.log(setUserInformationLoggedIn);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+
+    // 1= loggedIn, 0= notLoggedIn
+    localStorage.setItem("userIsLoggedIn", 1);
   };
+
+  useEffect(() => {
+    const setUserInformationLoggedIn = localStorage.getItem("userIsLoggedIn");
+    if (setUserInformationLoggedIn) setIsLoggedIn(1);
+  }, []);
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("userIsLoggedIn");
   };
 
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+//One place to manage a more nuanced state, if the user hasnt typed yet we get one state, if they blurred we get another depending on other states which we get with most updated versions
+const emailReducer = (state, action) => {
+  //Whatever is returned becomes our new state
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // useEffect(() => {
+  //   console.log("useEffect");
+
+  //   const cleaner = setTimeout(() => {
+  //     console.log(enteredEmail);
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     console.log("CLEANING");
+  //     clearInterval(cleaner);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
+
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    //if the dispatch function takes two args, is the object our state? Where is the action portion?
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
     );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setEnteredEmail(event.target.value);
+    setFormIsValid(enteredPassword.trim().length > 6 && emailState);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: "INPUT_BLUR" });
+    // setEmailIsValid(emailState.isValid);
   };
 
   const validatePasswordHandler = () => {
@@ -37,7 +71,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -45,21 +79,21 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
